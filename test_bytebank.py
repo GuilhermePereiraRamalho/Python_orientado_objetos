5cf9eda612c6ba3111d75c54fe4fbbcd4b8c921d
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-08-02 16:10:06 -0300

    Add files via upload

---
+++ test_bytebank.py
@@ -0,0 +1,55 @@
+import pytest
+from pytest import mark
+from codigo.bytebank import Funcionario
+
+
+class TestClass:
+
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
+        entrada = '13/03/2000' # Given - Contexto
+        esperado = 23
+
+        funcionario_teste = Funcionario('Teste', entrada, 1111)
+        resultado = funcionario_teste.idade() # When - Ação
+
+        assert resultado == esperado # Then-desfecho
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = ' Lucas Carvalho '
+        esperado = 'Carvalho'
+
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome()
+
+        assert resultado == esperado
+
+    def test_quando_descrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000
+        entrada_nome = 'Paulo Bragança'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario()
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada_salario = 1000
+        esperado = 100
+
+        funcionario_teste = Funcionario('teste', '11/11/2000', entrada_salario)
+        resultado = funcionario_teste.calcular_bonus()
+
+        assert resultado == esperado
+
+    @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_10000000_deve_retornar_exception(self):
+        with pytest.raises(Exception):
+            entrada_salario = 10000000
+
+            funcionario_teste = Funcionario('teste', '11/11/2000', entrada_salario)
+            resultado = funcionario_teste.calcular_bonus()
+
+            assert resultado
